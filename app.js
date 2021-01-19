eb8c197fc5d710a6cdb525bf97b3565de60aa53d
Branimir <branimir1111@gmail.com>
2021-01-19 21:52:43 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// ****************************************************************************************
+// Date
+// ****************************************************************************************
+
+// const months = ['Januar', 'Februar', 'Mart', 'April', 'Maj', 'Jun', 
+//                 'Jul', 'Avgust', 'Septembar', 'Oktobar', 'Novembar', 'Decembar'];
+                
+// const date = new Date();
+
+// const day = date.getDate();
+// const month = months[date.getMonth()];
+// const year = date.getFullYear();
+
+// console.log(`Danas je ${day}/${month}/${year}`);
+
+// ****************************************************************************************
+// Local storage
+// ****************************************************************************************
+
+// localStorage.setItem('name', 'bane');
+// sessionStorage.setItem('name', 'bane');
